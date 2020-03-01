fa7befe6fba15bd6f7617945b081d8a7526746b7
euan macqueen <euan_m108@hotmail.com>
2020-03-01 15:35:40 +0000

    Button

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,10 +1,12 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { MultiIntegrationButtonComponent } from '../multi-integration-button/multi-integration-button.component';
+
 
 @NgModule({
   imports: [
@@ -14,6 +16,7 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }])
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page,MultiIntegrationButtonComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class Tab3PageModule {}
